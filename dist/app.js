98d8c6b132639b9b30e4196bab852a48057067cb
mudit.k <mudit.k@muditks-MacBook-Pro.local>
2025-03-13 13:22:26 +0530

    Updated packages to latest versions

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = express_1.default();
+const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
     res.send('GET request is working');
